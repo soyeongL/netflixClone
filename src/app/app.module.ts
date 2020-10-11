c4bf30a5cd1f7b0bfcf36d2ce5165a564773a18c
soooooyeong <thdud6601@gmail.com>
2020-10-11 15:42:01 +0900

    where..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { AppRoutingModule } from './app-routing.module';
+import {SlickCarouselModule} from 'ngx-slick-carousel';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatBadgeModule,
+    AppRoutingModule,
+    SlickCarouselModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
